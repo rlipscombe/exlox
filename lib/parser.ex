807e4a720724a61aa54c7385695b23c26b4e65a9
Roger Lipscombe <roger@differentpla.net>
2020-02-20 22:04:42 +0000

    Support conjunction (&&)

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,7 +1,28 @@
+defmodule Exlox.Parser.Helpers do
+  @doc """
+  Converts a sequence of infix operators into a left-associative(?) AST.
+    Examples:
+    [true, :&&, false] is converted to {:&&, true, false}
+    [true, :&&, false, :&&, true] is converted to {:&&, [{:&&, [true, false]}, true]}
+  """
+  def fold_infixl(acc) do
+    acc
+    |> Enum.reverse()
+    |> Enum.chunk_every(2)
+    |> List.foldr([], fn
+      [l], [] -> l
+      [r, op], l -> {op, [l, r]}
+    end)
+  end
+end
+
 defmodule Exlox.Parser do
   import NimbleParsec
+  import Exlox.Parser.Helpers
 
   not_ = string("!") |> label("!")
+
+  and_ = string("&&") |> replace(:&&) |> label("&&")
 
   true_ = string("true") |> replace(true) |> label("true")
   false_ = string("false") |> replace(false) |> label("false")
@@ -11,5 +32,12 @@ defmodule Exlox.Parser do
 
   defcombinatorp(:factor, choice([negation, const]))
 
-  defparsec :parse, parsec(:factor)
+  defcombinatorp(
+    :term,
+    parsec(:factor)
+    |> repeat(and_ |> parsec(:factor))
+    |> reduce(:fold_infixl)
+  )
+
+  defparsec(:parse, parsec(:term))
 end
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -6,6 +6,7 @@ defmodule Exlox.ParserTest do
   end
 
   defp unwrap({:ok, [acc], "", _, _, _}), do: acc
+  defp unwrap({:ok, _, rest, _, _, _}), do: {:error, "could not parse" <> rest}
   defp unwrap({:error, reason, _rest, _, _, _}), do: {:error, reason}
 
   @err "expected boolean while processing !, followed by factor or boolean"
@@ -24,5 +25,12 @@ defmodule Exlox.ParserTest do
   test "parses double negations" do
     assert {:!, [{:!, [true]}]} == parse("!!true")
     assert {:!, [{:!, [false]}]} == parse("!!false")
+  end
+
+  test "parses conjunctions" do
+    assert {:&&, [true, false]} == parse("true&&false")
+    assert {:&&, [false, true]} == parse("false&&true")
+
+    assert {:&&, [{:&&, [true, false]}, true]} == parse("true&&false&&true")
   end
 end
