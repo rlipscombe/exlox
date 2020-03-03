0b63917b0ce2021048d02bfb08b5b925a7d2cf12
Roger Lipscombe <roger@differentpla.net>
2020-03-03 19:53:19 +0000

    Broke it

--- test/maths_parser_test.exs
+++ test/maths_parser_test.exs
@@ -14,6 +14,11 @@ defmodule Exlox.MathsParserTest do
   end
 
   test "simple subtraction" do
+    assert [{:-, [2, 3]}] == Exlox.MathsParser.parse("2-3") |> unwrap
+    assert [{:-, [2, {:-, [3, 4]}]}] == Exlox.MathsParser.parse("2-3-4") |> unwrap
+  end
+
+  test "simple multiplication" do
     assert [{:*, [2, 3]}] == Exlox.MathsParser.parse("2*3") |> unwrap
   end
 
